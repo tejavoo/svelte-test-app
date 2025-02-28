a36a853665b4361f9c915f43fd058c5b60eeecf3
tejays <saiteja@yourstory.com>
2025-02-28 11:52:27 +0530

    .env commit

---
+++ .svelte-kit/types/src/routes/proxy+layout.server.ts
@@ -0,0 +1,14 @@
+// @ts-nocheck
+// src/routes/+layout.server.ts
+import type { LayoutServerLoad } from './$types'
+
+
+export const load = async ({ locals: { safeGetSession }, cookies }: Parameters<LayoutServerLoad>[0]) => {
+  const { session, user } = await safeGetSession()
+
+  return {
+    session,
+    user,
+    cookies: cookies.getAll(),
+  }
+}
---
+++ .svelte-kit/types/src/routes/proxy+layout.ts
@@ -0,0 +1,42 @@
+// @ts-nocheck
+// src/routes/+layout.ts
+import { createBrowserClient, createServerClient, isBrowser } from '@supabase/ssr'
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
+import type { LayoutLoad } from './$types'
+import type { Session } from '@supabase/supabase-js'; // Import Session type
+
+export const load = async ({ fetch, data, depends }: Parameters<LayoutLoad>[0]) => {
+  depends('supabase:auth')
+
+  const supabase = isBrowser()
+    ? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+        global: {
+          fetch,
+        },
+      })
+    : createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+        global: {
+          fetch,
+        },
+        cookies: {
+          getAll() {
+            return data.cookies
+          },
+        },
+      })
+
+  /**
+   * It's fine to use `getSession` here, because on the client, `getSession` is
+   * safe, and on the server, it reads `session` from the `LayoutData`, which
+   * safely checked the session using `safeGetSession`.
+   */
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+  
+  // Access the user from the session, if available.
+  const user = session?.user || null;
+
+  // Return the session and user, matching the server-side load function.
+  return { supabase, session, user };
+};
